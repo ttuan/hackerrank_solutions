1e6d00a1097c596c683eb7a975c0213ffea5a129
ttuan <tuantv.nhnd@gmail.com>
2018-03-06 09:19:58 +0700

    30 days of code - day1 - data types

---
+++ 30-days-of-code/day1-data-types.rb
@@ -0,0 +1,13 @@
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+x = gets.chomp.to_i
+y = gets.chomp.to_f
+z = gets.chomp
+# Print the sum of both integer variables on a new line.
+puts x + i
+# Print the sum of the double variables on a new line.
+puts d + y
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+puts s + z
